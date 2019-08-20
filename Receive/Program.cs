2edc82299fde33489d0f1479a0cbecf4db4434ee
akshansha <akshanshagohil1995@gmail.com>
2019-08-20 19:04:57 +0530

    Add project files.

---
+++ Receive/Program.cs
@@ -0,0 +1,44 @@
+﻿//Consuming has a similar meaning to receiving. A consumer is a program that mostly waits to receive messages:
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Receive
+{
+    class Program
+    {
+        public static void Main()
+        {
+            //Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "Hello",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                    };
+                    channel.BasicConsume(queue: "Hello",
+                                         autoAck: true,
+                                         consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+
+                }
+            }
+
+        }
+    }
+}
---
+++ Send/Program.cs
@@ -0,0 +1,50 @@
+﻿//RabbitMQ is a message broker: it accepts and forwards messages. You can think about it as a post office
+//Producing means nothing more than sending. A program that sends messages is a producer
+
+
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Send
+{
+    class Program
+    {
+       static void Main()
+        {
+           
+            //Create a Connection To the server 
+            //ConnectionFactroy for construnt object
+            var factory = new ConnectionFactory() { HostName = "LocalHost" };
+            using (var connection = factory.CreateConnection())
+            {
+                //Create a Channel 
+                using (var channel = connection.CreateModel())
+                {
+                    //Declare a Queue for us to send
+                    //queue name must be same
+                    channel.QueueDeclare(queue: "Hello",
+                                          durable:false,
+                                          exclusive:false,
+                                          autoDelete:false,
+                                          arguments:null);
+
+
+                    //string message = "Hello From  Rushali !";
+                    Console.WriteLine("Enter Your Message u want to send ");
+                    string message = Console.ReadLine();
+                    var body = Encoding.UTF8.GetBytes(message);
+                    //Publish a message to the queue
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey:"Hello",
+                                         basicProperties:null,
+                                         body:body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+          }
+    }
+}
